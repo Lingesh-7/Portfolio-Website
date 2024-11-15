0f63fad3e3a517deb33fe70feacfc01a3ab3954c
Lingesh-7 <lingesh.r.official@gmail.com>
2024-11-15 11:30:48 +0530

    update project

--- main.py
+++ main.py
@@ -32,23 +32,23 @@ ckeditor=CKEditor(app)
 projects={'Box office model':{'repo':'https://github.com/Lingesh-7/Box-Office-Model.git','color':'danger'},
 'My Blog Website':{'repo':'https://github.com/Lingesh-7/MY-BLOG-WEBSITE.git','color':'warning'},
 'Twitter-News-Bot':{'repo':'https://github.com/Lingesh-7/Twitter-News-Bot-for-Puducherry.git','color':'info'},
-'PDF TO Audio\nconverter':{'repo':'https://github.com/Lingesh-7/PDF-TO-Audio-converter.git','color':'light'},
+'PDF TO Audio\nconverter':{'repo':'https://github.com/Lingesh-7/PDF-TO-Audio-converter.git','color':'dark'},
 
 'Real Estate Model':{'repo':'https://github.com/Lingesh-7/Real-Estate-Model.git','color':'danger'},
 'Cafe Website':{'repo':'https://github.com/Lingesh-7/Cafe-Website-with-API.git','color':'warning'},
 'Job application Bot':{'repo':'https://github.com/Lingesh-7/Job-application-Bot.git','color':'info'},
-'Snake-Game':{'repo':'https://github.com/Lingesh-7/Snake-Game.git','color':'light'},
+'Snake-Game':{'repo':'https://github.com/Lingesh-7/Snake-Game.git','color':'dark'},
 
 'Dr Semmelweis Analysis':{'repo':'https://github.com/Lingesh-7/Dr-Semmelweis-Analysis.git','color':'danger'},
 'Image Color Pallete':{'repo':'https://github.com/Lingesh-7/Image-Color-Pallete.git','color':'warning'},
 'Flipkart scraping':{'repo':'https://github.com/Lingesh-7/Flipkart-scraping-and-data-enrty.git','color':'info'},
-'Password manager':{'repo':'https://github.com/Lingesh-7/Advance-Password-manager.git','color':'light'},
+'Password manager':{'repo':'https://github.com/Lingesh-7/Advance-Password-manager.git','color':'dark'},
 
 
 'Nobel Prize Analysis':{'repo':'https://github.com/Lingesh-7/Nobel-Prize-Analysis.git','color':'danger'},
 'Top 10 Movies':{'repo':'https://github.com/Lingesh-7/Top-10-Movies.git','color':'warning'},
 'Quiz App':{'repo':'https://github.com/Lingesh-7/Advance-Quiz-App.git','color':'info'},
-'Rain Alert':{'repo':'https://github.com/Lingesh-7/Rain-Alert.git','color':'light'},
+'Rain Alert':{'repo':'https://github.com/Lingesh-7/Rain-Alert.git','color':'dark'},
 
 }
 @app.route('/')
